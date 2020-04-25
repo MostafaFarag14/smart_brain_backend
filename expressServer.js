2eed15f387489eb433f677c66c39cf116ac46178
mostafafarag <mostafafarag140@gmail.com>
2020-04-26 00:36:27 +0200

    change start script

--- expressServer.js
+++ expressServer.js
@@ -20,6 +20,9 @@ const db = knex({
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req,res)=>{
+  res.json('hello from server')
+})
 
 app.post('/login', (req, res) => login.handleLogin(req, res, db, bcrypt))
 
