23f7f5acdc5515cf7d762e44a6cab792b4641fc4
mostafafarag <mostafafarag140@gmail.com>
2020-04-26 15:46:51 +0200

    modify connection

--- expressServer.js
+++ expressServer.js
@@ -10,17 +10,15 @@ const app = express()
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '1234',
-    database: 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 })
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
   res.json('hello from server')
 })
 
@@ -30,6 +28,6 @@ app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt
 
 app.put('/image', (req, res) => image.handleImageDetect(req, res, db))
 
-app.listen( process.env.PORT || 3000 , () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`)
 })
