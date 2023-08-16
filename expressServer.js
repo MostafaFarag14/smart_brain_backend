819fe52db1b8aee47a16e49cc169d9745bcedaa2
MostafaFarag14 <mostafafarag140@gmail.com>
2023-08-16 06:11:57 +0300

    Remove build script

--- expressServer.js
+++ expressServer.js
@@ -1,34 +1,36 @@
-const cors = require('cors')
-const express = require('express')
-const bcrypt = require('bcryptjs')
-const knex = require('knex')
-const login = require('./controllers/login')
-const register = require('./controllers/register')
-const image = require('./controllers/image')
-const app = express()
+const cors = require("cors");
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
+const login = require("./controllers/login");
+const register = require("./controllers/register");
+const image = require("./controllers/image");
+const app = express();
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-  }
-})
+  },
+});
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json('hello from server')
-})
+app.get("/", (req, res) => {
+  res.json("hello from server");
+});
 
-app.post('/login', (req, res) => login.handleLogin(req, res, db, bcrypt))
+app.post("/login", (req, res) => login.handleLogin(req, res, db, bcrypt));
 
-app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
 
-app.put('/image', (req, res) => image.handleImageDetect(req, res, db))
+app.put("/image", (req, res) => image.handleImageDetect(req, res, db));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`)
-})
+  console.log(`app is running on port ${process.env.PORT}`);
+});
